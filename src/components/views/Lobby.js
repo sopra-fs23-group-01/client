8c2497a42df0279985609e1f4706e69b2356a473
zehao <zehao.zhang@uzh.ch>
2023-05-19 15:39:17 +0200

    backIcon

--- src/components/views/Lobby.js
+++ src/components/views/Lobby.js
@@ -29,8 +29,8 @@ const Lobby = () => {
     const [rooms, setRooms] = useState(null);
 
     const connect = () => {
-        //let Sock = new SockJS('http://localhost:8080/ws');
-        let Sock = new SockJS('https://sopra-fs23-group-01-server.oa.r.appspot.com/ws');
+        let Sock = new SockJS('http://localhost:8080/ws');
+        //let Sock = new SockJS('https://sopra-fs23-group-01-server.oa.r.appspot.com/ws');
         stompClient = over(Sock);
         stompClient.connect({}, onConnected, onError);
     }
