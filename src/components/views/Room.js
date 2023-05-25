147250e102c3e7062d44278b1531ba0b7a956dbe
Hanky Liu <103504158+zihanltesla@users.noreply.github.com>
2023-05-25 16:09:44 +0200

    Update Room.js

--- src/components/views/Room.js
+++ src/components/views/Room.js
@@ -269,8 +269,8 @@ const Room = () => {
     }, [room]);
     
     const connect = () => {
-        let Sock = new SockJS('http://localhost:8080/ws');
-        //let Sock = new SockJS('https://sopra-fs23-group-01-server.oa.r.appspot.com/ws');
+        //let Sock = new SockJS('http://localhost:8080/ws');
+        let Sock = new SockJS('https://sopra-fs23-group-01-server.oa.r.appspot.com/ws');
         stompClient = over(Sock);
         stompClient.connect({}, onConnected, onError);
     };
